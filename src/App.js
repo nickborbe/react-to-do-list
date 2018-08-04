1aa5cb3d1890ea734366f8438d82da9e332fb7c5
nickborbe <nickborbe@gmail.com>
2018-08-04 17:16:00 -0400

    edit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import AddTask from './addTask';
+import EditTask from './editTask'
 
 
 class App extends Component {
@@ -10,32 +11,57 @@ class App extends Component {
     super(props)
     this.state = {
       theTasks: [],
+      showing: false
     }
   }
-
 
   getAllTheTasks(){
     axios.get("http://localhost:5000/api/tasks")
     .then((allTheTasks)=>{
       // console.log(allTheTasks)
-      this.setState({theTasks: allTheTasks.data})
+      this.setState({theTasks: allTheTasks.data, showing: false})
     })
     .catch((err)=>{
       console.log(err)
     })
   }
 
+
+  toggleEditForm(whichTask){
+    if(this.state.showing === whichTask){
+      this.setState({theTasks: this.state.theTasks, showing: false});
+    } else{
+      this.setState({theTasks: this.state.theTasks, showing: whichTask});
+    }
+  }
+
+  renderForm(theIndex, theTaskID, theTitle, theDesc){
+    if(this.state.showing === theIndex){
+        return(
+          <EditTask blah={()=>this.getAllTheTasks()} taskProp={theTaskID} title={theTitle} desc={theDesc}></EditTask>
+      )
+    }
+  }
+
+
+
+
   showTasks(){
     if(this.state.theTasks.length === 0){
       this.getAllTheTasks();
     }
+    
 
     return (
-      this.state.theTasks.reverse().map(function(task, index){
+      this.state.theTasks.map((task, index) => {
         return(
       <div key={index}>
+        <button onClick={()=>this.toggleEditForm(index)} style={{float:'right', backgroundColor: 'greenyellow', padding: '10px'}}> 
+        Edit This Task 
+        </button>
       <h3>{task.title}</h3>
-      <p >{task.description} </p>
+      <p style={{maxWidth: '400px'}} >{task.description} </p>
+      {this.renderForm(index, task._id, task.title, task.description)}
       </div>
         ) 
       })
