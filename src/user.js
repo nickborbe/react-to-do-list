68e169a58d0cea1d4ebe39910aaa0e3cbd4073cb
nickborbe <nickborbe@gmail.com>
2018-08-11 13:26:19 -0400

    adds button stylings

--- src/user.js
+++ src/user.js
@@ -8,6 +8,7 @@ class User extends Component {
         this.state = {
             usernameInput: '',
             passwordInput: '',
+            loggedInUser: null,
         }
     }
     updatePassword(e){
@@ -30,22 +31,32 @@ class User extends Component {
         const password = this.state.passwordInput;
         axios.post(`http://localhost:5000/api/login`, {username, password }, {withCredentials: true})
         .then((response)=>{
-            console.log(response.data);
+            this.setState({
+                usernameInput: this.state.usernameInput,
+                passwordInput: this.state.passwordInput,
+                loggedInUser:  response.data,
+            })
+           
         })
 
+    }
+
+    showUser(){
+       return this.state.loggedInUser? `Welcome, ${this.state.loggedInUser.username}` : 'User Component'
     }
 
     render(){
         return(
             <div>
-            <h3> User Component </h3>
+            <h3> {this.showUser()} </h3>
             {/* <div>username here</div> */}
             <label> Username </label>
             <input value = {this.state.usernameInput} onChange={(e)=>{this.updateUsername(e)}} type="text"/>
             <label> Password </label>
             <input value = {this.state.passwordInput} onChange={(e)=>{this.updatePassword(e)}} type="password"/>
             
-            <button onClick={()=>{this.login()}} > Log In </button>
+            <button  onClick={()=>{this.login()}} className="little-green-btn" > Log In </button>
+            <button onClick={()=>{this.login()}} className="little-green-btn" > Create New Account </button>
             </div>
         )
     }
