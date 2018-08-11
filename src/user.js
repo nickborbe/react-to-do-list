f4382968f31ae0904ef2598b639246f47c5cda4d
nickborbe <nickborbe@gmail.com>
2018-08-11 13:16:08 -0400

    adding login feature

--- src/user.js
+++ src/user.js
@@ -6,8 +6,33 @@ class User extends Component {
     constructor(props){
         super(props)
         this.state = {
-    
+            usernameInput: '',
+            passwordInput: '',
         }
+    }
+    updatePassword(e){
+        this.setState({
+            usernameInput: this.state.usernameInput,
+            passwordInput: e.target.value,
+        })
+       
+ 
+    }
+    updateUsername(e){
+            this.setState({
+                usernameInput: e.target.value,
+                passwordInput: this.state.passwordInput,
+            })
+    }
+
+    login(){
+        const username = this.state.usernameInput;
+        const password = this.state.passwordInput;
+        axios.post(`http://localhost:5000/api/login`, {username, password }, {withCredentials: true})
+        .then((response)=>{
+            console.log(response.data);
+        })
+
     }
 
     render(){
@@ -16,10 +41,11 @@ class User extends Component {
             <h3> User Component </h3>
             {/* <div>username here</div> */}
             <label> Username </label>
-            <input type="text"/>
+            <input value = {this.state.usernameInput} onChange={(e)=>{this.updateUsername(e)}} type="text"/>
             <label> Password </label>
-            <input type="password"/>
+            <input value = {this.state.passwordInput} onChange={(e)=>{this.updatePassword(e)}} type="password"/>
             
+            <button onClick={()=>{this.login()}} > Log In </button>
             </div>
         )
     }
